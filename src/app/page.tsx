6b41c420953b097c2f7eb218e5cacecf0315bef2
ch00z00 <choozoo10@gmail.com>
2023-12-12 07:32:21 +0900

    [add] First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,83 @@
+'use client'
+
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default function Home() {
-  console.log(OrbitControls)
+  let canvas: HTMLElement
+  useEffect(() => {
+    if (canvas) return
+    // canvasを取得
+    canvas = document.getElementById('canvas')!
+
+    // シーン
+    const scene = new THREE.Scene()
+
+    // サイズ
+    const sizes = {
+      width: innerWidth,
+      height: innerHeight
+    }
+
+    // カメラ
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      1000
+    )
+
+    // レンダラー
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas || undefined,
+      antialias: true,
+      alpha: true
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    // ボックスジオメトリー
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+    const boxMaterial = new THREE.MeshLambertMaterial({
+      color: '#2497f0'
+    })
+    const box = new THREE.Mesh(boxGeometry, boxMaterial)
+    box.position.z = -5
+    box.rotation.set(10, 10, 10)
+    scene.add(box)
+
+    // ライト
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+    scene.add(ambientLight)
+    const pointLight = new THREE.PointLight(0xffffff, 0.2)
+    pointLight.position.set(1, 2, 3)
+    scene.add(pointLight)
+
+    // アニメーション
+    const clock = new THREE.Clock()
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime()
+      box.rotation.x = elapsedTime
+      box.rotation.y = elapsedTime
+      window.requestAnimationFrame(tick)
+      renderer.render(scene, camera)
+    }
+    tick()
+
+    // ブラウザのリサイズ処理
+    window.addEventListener('resize', () => {
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(window.devicePixelRatio)
+    })
+  }, [])
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="flex w-[min(90vw)] h-[min(40vw)] items-center justify-center rounded-3xl bg-white-200">
-        <div className="text-black-100 text-[min(20vw)] tracking-tighter animate-[pulse_2s_ease-in-out_infinite]">Annvision</div>
-        <div className="text-black-100 text-[min(20vw)] tracking-tighter"></div>
-      </div>
-    </div>
+    <>
+      <canvas id="canvas"></canvas>
+    </>
   )
 }
